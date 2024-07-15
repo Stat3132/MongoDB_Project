26aa1225924896787c3e1876a4d21d61889504e1
Stat3132 <starxstar1016@gmail.com>
2024-07-15 11:02:22 -0600

    This is just a test

---
+++ src/main/java/CSC180/Perez/Diego/Main.java
@@ -0,0 +1,9 @@
+package CSC180.Perez.Diego;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 5;
+        System.out.println(x);
+    }
+}
