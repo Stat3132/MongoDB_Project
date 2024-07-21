08a03f71c3f47ee93ffc15e704d9d184f2ad6c3f
Stat3132 <starxstar1016@gmail.com>
2024-07-21 14:19:22 -0600

    Added comments to connect to make it more cohesive.

--- src/main/java/Controller/MongoController.java
+++ src/main/java/Controller/MongoController.java
@@ -17,22 +17,29 @@ public class MongoController {
 
     public void mongoControl() {
         mongoClient = connectToMongo();
+        //Global variables for my database and collection
         database = mongoClient.getDatabase("EmployeeDatabase");
         collection = database.getCollection("EmployeeCollection");
 
     }
     public MongoClient connectToMongo(){
+        //Connects to my local MongoDB server
         String connectionString = "mongodb://localhost:27017/";
+        //API to be able to access server
         ServerApi serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
+        //Setting up the connection and logic related to the connection
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(connectionString))
                 .serverApi(serverApi)
                 .build();
 
+        //Creating the mongo DB connection with these settings.
         MongoClient mongoClient = MongoClients.create(settings);
+        //Solidifying the database I am trying to access
         MongoDatabase database = mongoClient.getDatabase("EmployeeDatabase");
+        //Ping just to makes sure database is implemented correctly
         database.runCommand(new Document("ping", 1));
         System.out.println("Connected to MongoDB!");
         return mongoClient;
