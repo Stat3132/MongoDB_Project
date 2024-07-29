6e7057ab740354c5b564476c2e0bf17d96748613
Stat3132 <starxstar1016@gmail.com>
2024-07-29 14:56:12 -0600

    Methods for Neo4J

--- src/main/java/Controller/Neo4JController.java
+++ src/main/java/Controller/Neo4JController.java
@@ -5,6 +5,22 @@ import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Session;
 
 public class Neo4JController {
+    public void connectToNeo4J(){
+
+    }
+
+    public void addIntoNeo4J(){
+
+    }
+    public void deleteFromNeo4J() {
+
+    }
+    public void updateFromNeo4J(){
+
+    }
+    public void readFromNeo4J(){
+
+    }
 
 
 }
