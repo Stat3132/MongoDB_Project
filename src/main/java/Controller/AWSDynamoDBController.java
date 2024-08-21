34a5cf13f673841afbad25ad73a08cdacf8f381e
Tomonator1000 <tommysmith12443@gmail.com>
2024-08-21 10:56:20 -0600

    I DID IT

---
+++ src/main/java/Controller/AWSDynamoDBController.java
@@ -0,0 +1,10 @@
+/**
+ * @author tsoutherland
+ * @createdOn 8/21/2024 at 10:56 AM
+ * @projectName MongoDB_Project
+ * @packageName Controller;
+ */
+package Controller;
+
+public class AWSDynamoDBController {
+}
