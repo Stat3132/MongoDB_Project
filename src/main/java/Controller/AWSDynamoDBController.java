19a15462420e17020c26501038fcc05d4f27f1f2
Tomonator1000 <tommysmith12443@gmail.com>
2024-08-25 20:00:18 -0600

    I DID IT

--- src/main/java/Controller/AWSDynamoDBController.java
+++ src/main/java/Controller/AWSDynamoDBController.java
@@ -6,5 +6,49 @@
  */
 package Controller;
 
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import java.net.URI;
+
 public class AWSDynamoDBController {
+
+    public AWSDynamoDBController(){
+        buildConnection();
+    }
+
+
+    public void buildConnection(){
+        // Configure DynamoDB Local endpoint
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:8000"))
+                .httpClient(UrlConnectionHttpClient.builder().build())
+                .region(Region.US_EAST_1)  // Region is required but ignored in local mode
+                .build();
+
+        // Example: List tables
+        ListTablesRequest request = ListTablesRequest.builder().build();
+        ListTablesResponse response = ddb.listTables(request);
+
+        System.out.println("Tables:");
+        response.tableNames().forEach(System.out::println);
+
+        try {
+            ScanRequest scanRequest = ScanRequest.builder()
+                    .tableName("Person")
+                    .build();
+
+            ScanResponse scanResponse = ddb.scan(scanRequest);
+
+            System.out.println("Items in table " + "Person" + ":");
+            scanResponse.items().forEach(item -> {
+                item.forEach((key, value) -> System.out.println(key + ": " + value.s()));
+            });
+
+        } catch (DynamoDbException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Controller.AWSDynamoDBController;
 import Controller.MongoController;
 import Controller.Neo4JController;
 import Controller.PersonController;
@@ -6,9 +7,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        PersonController personController = new PersonController();
-        personController.startUp();
-        Neo4JController neo4j = new Neo4JController();
+        //PersonController personController = new PersonController();
+        //personController.startUp();
+        //Neo4JController neo4j = new Neo4JController();
+        AWSDynamoDBController awsDynamoDBController = new AWSDynamoDBController();
        // MongoController mongoController = new MongoController();
        // mongoController.closeMongo();
     }
